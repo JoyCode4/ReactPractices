5072647e9e83f897e8cc4054b2786d8b7dc557ec
JoyCode4 <joywadhonkar4@gmail.com>
2023-04-20 13:03:07 +0530

    styling again and add icon but it is not working

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,31 @@
 import React from "react";
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            prices:999,
+            title:"Mobile Phone",
+            qty:1,
+            img:""
+        }
+    }
     render(){
+        const {prices,title, qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div>Phone</div>
-                    <div>Rs 999</div>
-                    <div>Qyt : 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:"grey"}}> Rs : {prices} </div>
+                    <div style={{color:"grey"}}> Qyt : {qty} </div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img alt="increase" className="action-icons" src="https://www.flaticon.com/free-icon/add_992651?term=add&page=1&position=1&origin=search&related_id=992651"/>
+                        <img alt="decrease" className="action-icons" src="https://fontawesome.com/v5/icons/link?f=classic&s=solid"/>
+                        <img alt="delete" className="action-icons" src="https://www.flaticon.com/free-icon/delete_1214428?term=delete&page=1&position=1&origin=search&related_id=1214428"/>
                     </div>
                 </div>
             </div>
