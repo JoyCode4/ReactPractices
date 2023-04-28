ebe6e88140fa108548458bfb12f71ddc6599f712
JoyCode4 <joywadhonkar4@gmail.com>
2023-04-28 09:15:48 +0530

    added icon using react-icons npm library and also use the Fpnt Awasome icon through that

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaPlusCircle,FaMinusCircle,FaTrashAlt } from "react-icons/fa";
 
 class CartItem extends React.Component{
     constructor(){
@@ -56,9 +57,12 @@ class CartItem extends React.Component{
                     <div style={{color:"grey"}}> Qyt : {qty} </div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt="increase" onClick={this.increaseQuantity} className="action-icons" src="https://www.flaticon.com/free-icon/add_992651?term=add&page=1&position=1&origin=search&related_id=992651"/>
-                        <img alt="decrease" onClick={this.decreaseQuantity} className="action-icons" src="https://fontawesome.com/v5/icons/link?f=classic&s=solid"/>
-                        <img alt="delete" className="action-icons" src="https://www.flaticon.com/free-icon/delete_1214428?term=delete&page=1&position=1&origin=search&related_id=1214428"/>
+                        {/* <img alt="increase" onClick={this.increaseQuantity} className="action-icons" src="https://www.flaticon.com/free-icon/add_992651?term=add&page=1&position=1&origin=search&related_id=992651"/> */}
+                        {/* <img alt="decrease" onClick={this.decreaseQuantity} className="action-icons" src="https://fontawesome.com/v5/icons/link?f=classic&s=solid"/> */}
+                        {/* <img alt="delete" className="action-icons" src="https://www.flaticon.com/free-icon/delete_1214428?term=delete&page=1&position=1&origin=search&related_id=1214428"/> */}
+                        <a className="action-icons" onClick={this.increaseQuantity}><FaPlusCircle/></a>
+                        <a className="action-icons" onClick={this.decreaseQuantity}><FaMinusCircle/></a>
+                        <a className="action-icons"><FaTrashAlt/></a>
                     </div>
                 </div>
             </div>
